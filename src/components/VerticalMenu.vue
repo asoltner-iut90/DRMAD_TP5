ec953f786bd4b1c8b79106f051f873cf1c3b31ac
Quoruda <audrick.soltner@proton.me>
2025-01-12 00:05:22 +0100

    Add VerticalMenu

---
+++ src/components/VerticalMenu.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="vertical-menu">
+    <ul>
+      <li v-for="(item, index) in items" :key="index" >
+        <slot v-if="item.type ==='type'" name="menu-title" :label="item.label">{{item.label}}</slot>
+        <span v-else-if="item.type==='link'" @click="goTo(item.to)">
+          <slot name="menu-link" :label="item.label">
+              <button>{{item.label}}</button>
+          </slot>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'VerticalMenu',
+    methods: {
+      goTo(path){
+        this.$router.push(path);
+      }
+    },
+    props: { items: Array },
+
+  }
+</script>
+
+<style scoped>
+
+</style>
