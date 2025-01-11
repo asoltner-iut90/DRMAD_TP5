22daeab191b6f2fd48dbeb3ee342a155282ba964
Quentin Belhadj <quentin.belhadj@edu.univ-fcomte.fr>
2025-01-11 22:25:42 +0100

    12°/ Déconnexion

---
+++ src/views/BankLogout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bank-logout">
+    <p>Déconnexion en cours...</p>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'BankLogout',
+  methods: {
+    ...mapActions('bank', ['clearCurrentAccount']),
+  },
+  mounted() {
+    setTimeout(async () => {
+      await this.clearCurrentAccount();
+      this.$router.push('/bank/home');
+    }, 1000);
+  },
+};
+</script>
+
+<style scoped>
+.bank-logout {
+  text-align: center;
+  margin-top: 50px;
+  font-size: 20px;
+}
+</style>
